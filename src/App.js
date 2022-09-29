e07eb53b47906fd02d89e8b473b215bc7d82c05e
jenia0jenia <jenia0jenia@gmail.com>
2022-09-29 11:36:14 +0300

    first props, state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+    const [value, setValue] = useState('')
+    console.log(value);
+    console.log(count);
+
+    function inc() {
+        setCount(count + 1)
+    }
+
+    function dec() {
+        setCount(count - 1)
+    }
+
+    return (
+        <div className="App">
+            <header>{count}</header>
+            <h1>{value}</h1>
+            <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+            <button onClick={inc}>++</button>
+            <button onClick={dec}>--</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
